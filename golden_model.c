5eddf372971093154709822d6e504a761c46189d
xiranw <43282951+xiranw@users.noreply.github.com>
2021-10-09 00:58:17 -0400

    Golden model: added logic to process all instr types

--- golden_model.c
+++ golden_model.c
@@ -1,9 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum opcode {
+    MOV  = 0x0,
+    ADD  = 0x1,
+    SUB  = 0x2,
+    AND  = 0x3,
+    OR   = 0x4,
+    XOR  = 0x5,
+    SLL  = 0x6,
+    SRL  = 0x7,
+    SRA  = 0x8,
+    ADDI = 0x9,
+    ANDI = 0xa,
+    ORI  = 0xb,
+    XORI = 0xc,
+    SLLI = 0xd,
+    SRLI = 0xe,
+    SRAI = 0xf
+} opcode_t;
+
 void process_instr(char *line, int16_t *reg_file) {
-    int16_t instr = strtol(line, NULL, 16); // convert char * to int16_t
-    printf("%x\n", instr);
+    int16_t instr, opcode, dest_reg, src1_reg, src2_reg, src2_imm;
+
+    instr = strtol(line, NULL, 16); // convert char * to int16_t
+
+    /* Decode instr */
+    opcode = (instr >> 12) & 0xF;
+    dest_reg = (instr >> 8) & 0xF;
+    src1_reg = (instr >> 4) & 0xF;
+    src2_reg = instr & 0xF;
+    if (src2_reg & 0x8) src2_imm = src2_reg | 0xFFF0; // sign-extend src2_imm
+    else src2_imm = src2_reg;
+    
+    printf("instr:%hx, opcode:%hx, dest_reg:%hx, src1_reg:%hx, src2_reg:%hx, src2_imm:%hx\n", 
+    instr, opcode, dest_reg, src1_reg, src2_reg, src2_imm);
+
+    /* Perform computation */
+    switch(opcode) {
+        case MOV:  {
+            reg_file[dest_reg] = reg_file[src1_reg];
+            break;
+        }
+
+        case ADD:  {
+            reg_file[dest_reg] = reg_file[src1_reg] + reg_file[src2_reg];
+            break;
+        }
+
+        case SUB:  {
+            reg_file[dest_reg] = reg_file[src1_reg] - reg_file[src2_reg];
+            break;
+        }
+
+        case AND:  {
+            reg_file[dest_reg] = reg_file[src1_reg] & reg_file[src2_reg];
+            break;
+        }
+
+        case OR:   {
+            reg_file[dest_reg] = reg_file[src1_reg] | reg_file[src2_reg];
+            break;
+        }
+
+        case XOR:  {
+            reg_file[dest_reg] = reg_file[src1_reg] ^ reg_file[src2_reg];
+            break;
+        }
+
+        case SLL:  {
+            reg_file[dest_reg] = reg_file[src1_reg] << (reg_file[src2_reg] & 0xF); // can only shift by 0 ~ 15
+            break;
+        }
+
+        case SRL:  {
+            // cast to unsigned for logical shift
+            reg_file[dest_reg] = (uint16_t)reg_file[src1_reg] >> (reg_file[src2_reg] & 0xF); // can only shift by 0 ~ 15
+            break;
+        }
+
+        case SRA:  {
+            reg_file[dest_reg] = reg_file[src1_reg] >> (reg_file[src2_reg] & 0xF); // can only shift by 0 ~ 15
+            break;
+        }
+
+        case ADDI: {
+            reg_file[dest_reg] = reg_file[src1_reg] + src2_imm;
+            break;
+        }
+
+        case ANDI: {
+            reg_file[dest_reg] = reg_file[src1_reg] & src2_imm;
+            break;
+        }
+
+        case ORI:  {
+            reg_file[dest_reg] = reg_file[src1_reg] | src2_imm;
+            break;
+        }
+
+        case XORI: {
+            reg_file[dest_reg] = reg_file[src1_reg] ^ src2_imm;
+            break;
+        }
+
+        case SLLI: {
+            reg_file[dest_reg] = reg_file[src1_reg] << (src2_imm & 0xF); // can only shift by 0 ~ 15
+            break;
+        }
+
+        case SRLI: {
+            // cast to unsigned for logical shift
+            reg_file[dest_reg] = (uint16_t) reg_file[src1_reg] >> (src2_imm & 0xF); // can only shift by 0 ~ 15
+            break;
+        }
+
+        case SRAI: {
+            reg_file[dest_reg] = reg_file[src1_reg] >> (src2_imm & 0xF); // can only shift by 0 ~ 15
+            break;
+        }
+
+        default:   {
+            fprintf(stderr, "Encountered unknown opcode %hx", opcode);
+            exit(-1);
+        }
+    }
+
+    reg_file[0] = 0; // r0 = 0 does not change
 }
 
 int main(int argc, char *argv[]) {
@@ -27,12 +150,15 @@ int main(int argc, char *argv[]) {
     char *line = NULL;
     size_t line_size;
     while ((read = getline(&line, &line_size, fp)) != -1) { // stop at end-of-file
-        if (read != 3) { // 2 bytes for instr + 1 byte for new line char
+        if (read != 5) { // 4 bytes for instr + 1 byte for new line char
             fprintf(stderr, "Error: Encountered %zu-byte line when each line should be 3 bytes\n", read);
             exit(-1);
         }
         process_instr(line, reg_file);
     }
+
+    for (int i = 0; i < 16; i++)
+        printf("%x: %hx\n", i, reg_file[i]);
 
     free(line);
     fclose(fp);
