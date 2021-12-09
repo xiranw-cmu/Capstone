7b454719075f9bd6facc967d6e15d807232767ce
alihoffmann <alihoffmann0@gmail.com>
2021-12-09 15:35:56 -0500

    completed python sending and receiving code

--- UART_PI/serial_read.py
+++ UART_PI/serial_read.py
@@ -4,24 +4,35 @@ import serial
 
 ser = serial.Serial(
         port='/dev/ttyUSB0',
-        baudrate = 921600,
+        baudrate = 576000,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
         timeout=1
 )
-count = 0
-t = 0
-temp = 0
-while 1:
-    x=ser.read(21)
-    if x != b'' and count == 0:
-        t = time.time()
-        print(t)
-    if x != b'':
-        count+=1
-        print(x, count, " ")
-    if count == 2859:
-        temp = time.time()
-        print(temp-t)
-        break
+
+## ali needs to figure out how many test cases there are
+file_name = 'sim_output.txt'
+test_cases = sys.argv[2]
+prev = ''
+with open (file_name, "w") as f:
+    t = 0
+    temp = 0
+    while 1:
+        x=ser.read(3)
+        line = x.hex() ## hex string formatted like DATA-DATA-REG-0000
+        result = f"{line[4]}{line[0:3]}"
+        if x != b'' and count == 0:
+            t = time.time()
+            print(t)
+        if x != b'':
+            count+=1
+            prev = x
+            f.write(result + "\n")
+            print(x, count, " ")
+        if count == test_cases:
+            temp = time.time()
+            print(temp-t)
+            break
+        if x == b'' and prev == b'' and count > 0:
+            print("OH NO!!")
--- UART_PI/serial_write.py
+++ UART_PI/serial_write.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 import time
 import serial
+import os
 
 ser = serial.Serial(
-        port='/dev/ttyUSB0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
-        baudrate = 921600,
+        port='/dev/ttyUSB0',
+        baudrate = 576000,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
         timeout=1
 )
-counter=0
-
-for i in range(2860):
-        ser.write(bytes('Ab Cd Ef Gh Ij Kl MnH', 'ascii'))
-        time.sleep(1e-6)
-        #counter += 1
+file_name = ''
+if (len(sys.argv) < 2):
+        file_name = "test_case.txt"
+else:
+        file_name = sys.argv[2]
+with open(file_name) as test_fd:
+        test_cases = test_fd.splitlines()
+        os.system('python serial_read.py %d', len(test_cases))
+        for i in range(len(test_cases)):
+                test = bytes.fromhex(f"{test_cases[i]}00")
+                ser.write(test)
